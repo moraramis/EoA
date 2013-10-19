20c161c366b286f8183c4ebe7ba617a3319ac62a
Josh Klug <jklug2001@fullsail.edu>
2013-10-19 17:26:09 -0400

    Updated db.php COMMENTS
    Updated to reflect current db in use. Renamed the Fields array items to
    match the db.

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 //My Database Connection info
-const MY_DSN = "mysql:host=127.0.0.1;port=3306;dbname=interestpoint";
+const MY_DSN = "mysql:host=127.0.0.1;port=3306;dbname=eoa";
 const MY_USER = "root";
 const MY_PASS = "";
 
@@ -9,9 +9,10 @@ $db = new \PDO(MY_DSN, MY_USER, MY_PASS);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 $fields = array(
-    'userName'=>'User Name',
-    'firstName'=>'First Name',
-    'lastName'=>'Last Name',
+    'uname'=>'User Name',
+    'fname'=>'First Name',
+    'lname'=>'Last Name',
     'email'=>'Email',
+    'password'=>'Password',
 );
 ?>
